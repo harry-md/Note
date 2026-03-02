d0d90124c40e8672376f0426959542e497046e1a
harry-md <maoduchai404@gmail.com>
2026-03-02 17:27:24 +0700

    update

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
 			provider: "plausible",
 		},
 		locale: "en-US",
-		baseUrl: "quartz.jzhao.xyz",
+		baseUrl: "harry-md.github.io/Note/",
 		ignorePatterns: ["private", "templates", ".obsidian"],
 		defaultDateType: "modified",
 		theme: {
